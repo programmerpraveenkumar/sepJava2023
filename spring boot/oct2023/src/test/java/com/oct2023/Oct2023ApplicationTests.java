0dba5bfd6c8a2e8fb73caef9a37e4ebf5e689287
praveen kumar <programmerpraveenkumar@gmail.com>
2023-10-02 22:11:13 +0530

    jpa and databse

---
+++ spring boot/oct2023/src/test/java/com/oct2023/Oct2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oct2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oct2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
